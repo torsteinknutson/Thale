1a862894764505b6a32bb3521d67fb1c45825892
Torstein Knutson - a5050re <torstein.knutson@fremtind.no>
2026-01-12 12:29:56 +0100

    fix: add React ESLint plugin to properly detect JSX usage
    - Install and configure eslint-plugin-react
    - Enable React rules that understand JSX component usage
    - Disable react-in-jsx-scope (not needed in React 17+)
    - Disable prop-types (not using PropTypes validation)
    - This will eliminate false positive warnings about unused imports in JSX files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
@@ -17,17 +18,26 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+      'react/prop-types': 'off', // We're not using prop-types
       'no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ]
